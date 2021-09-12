18ce948e63b1f62b456d4fdc42f3342f351bdc7e
Underscore76 <theunderscore76@gmail.com>
2021-09-12 15:37:44 -0500

    fix for invincibility, transition to desert

---
+++ BattleRoyale/Farmer_MovePosition.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Xna.Framework;
+using StardewValley;
+namespace BattleRoyale
+{
+    class Farmer_MovePosition : Patch
+    {
+        // I always want to trigger that a farmer's invuln timer should decrease
+        protected override PatchDescriptor GetPatchDescriptor() => new PatchDescriptor(typeof(Farmer), "MovePosition");
+
+        public static void Postfix(GameTime time, ref Farmer __instance)
+        {
+            if (__instance.temporarilyInvincible)
+            {
+                __instance.temporaryInvincibilityTimer += time.ElapsedGameTime.Milliseconds;
+                if (__instance.temporaryInvincibilityTimer > 1200)
+                {
+                    __instance.temporarilyInvincible = false;
+                    __instance.temporaryInvincibilityTimer = 0;
+                }
+            }
+        }
+    }
+}
